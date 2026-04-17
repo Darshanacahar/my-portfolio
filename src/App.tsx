82ee5b3d45c68c39b52e8a977ebd131473f979e6
Darshanacahar <Darshanacahar@users.noreply.github.com>
2026-04-17 15:34:53 +0530

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <SmoothScroll />
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <BrowserRouter basename={import.meta.env.BASE_URL} future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
